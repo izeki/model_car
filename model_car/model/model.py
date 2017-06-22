8187f73f01bc0142d731be73793fe0e8cd77346d
bdd <bdd@rfs454-122-002.path.berkeley.edu>
2017-06-22 09:52:10 -0700

    keras

--- model_car/model/model.py
+++ model_car/model/model.py
@@ -297,7 +297,7 @@ def conv2Dgroup(filters, kernel_size, strides, padding, activation, name, data_f
 
 def get_model(channel=3, meta_label=6, input_width=672, input_height=376, phase='train'):
     ZED_data = Input(shape=(channel*4,input_height, input_width), name='ZED_data')
-    metadata =  Input(shape=(meta_label, 26, 13), name='metadata')
+    metadata =  Input(shape=(meta_label, 13,  26), name='metadata')
     steer_motor_target_data = Input(shape=(1,20), name='steer_motor_target_data')    
     
     ZED_data_pool1 = AveragePooling2D(pool_size=(3, 3), strides=(2,2), padding='valid', data_format='channels_first', name='ZED_data_pool1')(ZED_data)
