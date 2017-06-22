78311ab27398539b6876109ffbd9b8f26c928384
bdd <bdd@rfs454-122-002.path.berkeley.edu>
2017-06-21 22:38:00 -0700

    keras

--- model_car/model/model.py
+++ model_car/model/model.py
@@ -1,5 +1,5 @@
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, Dense, concatenate
+from keras.layers import Input, BatchNormalization, AveragePooling2D, Conv2D, MaxPooling2D, Dense, concatenate
 from keras import regularizers
 from keras.layers import Activation
 import keras as k
@@ -8,42 +8,108 @@ import keras as k
 """
 Karl's model car model:
 name: "z2_color_4_layers"
-input: "ZED_data_pool2"
-input_shape {
-    dim: 1
-    dim: 12
-    dim: 94
-    dim: 168
+layer {
+	name: "steer_motor_target_data"
+	type: "DummyData"
+	top: "steer_motor_target_data"
+	dummy_data_param {
+		shape {
+			dim: 1
+			dim: 20
+		}
+	}
 }
 
-input: "metadata"
-input_shape {
-    dim: 1
-    dim: 6
-    dim: 14
-    dim: 26
+layer {
+        name: "metadata"
+        type: "DummyData"
+        top: "metadata"
+        dummy_data_param {
+                shape {
+                        dim: 1
+                        dim: 6
+                        dim: 14
+                        dim: 26
+                }
+        }
 }
 
-input: "steer_motor_target_data"
-input_shape {
-    dim: 1
-    dim: 20
+layer {
+	name: "ZED_data"
+	type: "DummyData"
+	top: "ZED_data"
+	dummy_data_param {
+		shape {
+			dim: 1
+			dim: 12
+			dim: 376
+			dim: 672
+		}
+	}
 }
 
+layer {
+	name: "ZED_data_pool1"
+	type: "Pooling"
+	bottom: "ZED_data" #"MVN" #
+	top: "ZED_data_pool1"
+	pooling_param {
+		pool: AVE
+		kernel_size: 3
+		stride: 2
+		pad: 0
+	}
+}
+
+layer {
+	name: "ZED_data_pool2"
+	type: "Pooling"
+	bottom: "ZED_data_pool1"
+	top: "ZED_data_pool2"
+	pooling_param {
+		pool: AVE
+		kernel_size: 3
+		stride: 2
+		pad: 0
+	}
+}
+
+layer {
+  name: "ZED_data_pool2_scale"
+  type: "Scale"
+  bottom: "ZED_data_pool2"
+  top: "ZED_data_pool2_scale"
+  param {
+    lr_mult: 0
+    decay_mult: 0
+  }
+  param {
+    lr_mult: 0
+    decay_mult: 0
+  }
+  scale_param {
+    filler {
+      value: 0.003921    }
+    bias_term: true
+    bias_filler {
+      value: -0.5
+    }
+  }
+}
+	
 ###################### Convolutional Layer Set 'conv1' ######################
 #
 layer {
 	name: "conv1"
 	type: "Convolution"
-	bottom: "ZED_data_pool2"
+	bottom: "ZED_data_pool2_scale"
 	top: "conv1"
 	convolution_param {
 		num_output: 96
 		group: 1
 		kernel_size: 11
 		stride: 3
-		pad_h: 0
-		pad_w: 0
+		pad: 0
 		weight_filler {
 			type: "gaussian" 
 			std: 0.00001
@@ -67,27 +133,24 @@ layer {
 		pool: MAX
 		kernel_size: 3
 		stride: 2
-		pad_h: 0
-		pad_w: 0
+		pad: 0
 	}
 }
 	
-#
 ############################################################
 
-
-
 layer {
-	type: 'Concat'
-	name: 'conv1_metadata_concat'
-	bottom: "conv1_pool"
-	bottom: "metadata"
-	top: 'conv1_metadata_concat'
-	concat_param {
-		axis: 1
-	}
-}
-		
+  name: "conv1_metadata_concat"
+  type: "Concat"
+  bottom: "conv1_pool"
+  bottom: "metadata"
+  top: "conv1_metadata_concat"
+  concat_param {
+    axis: 1
+  }     
+}               
+                        
+
 
 ###################### Convolutional Layer Set 'conv2' ######################
 #
@@ -101,8 +164,7 @@ layer {
 		group: 2
 		kernel_size: 3
 		stride: 2
-		pad_h: 0
-		pad_w: 0
+		pad: 0
 		weight_filler {
 			type: "gaussian" 
 			std: 0.1
@@ -126,14 +188,15 @@ layer {
 		pool: MAX
 		kernel_size: 3
 		stride: 2
-		pad_h: 0
-		pad_w: 0
+		pad: 0
 	}
 }
 	
-#
 ############################################################
 
+
+###################### IP Layer Set 'ip1' ######################
+#
 layer {
 	name: "ip1"
 	type: "InnerProduct"
@@ -146,14 +209,19 @@ layer {
 		}
 	}
 }
-
+	
 layer {
 	name: "ip1_relu"
 	type: "ReLU"
 	bottom: "ip1"
 	top: "ip1"
 }
+	
+############################################################
 
+
+###################### IP Layer Set 'ip2' ######################
+#
 layer {
 	name: "ip2"
 	type: "InnerProduct"
@@ -166,16 +234,6 @@ layer {
 		}
 	}
 }
-
-layer {
-	name: "euclidean"
-	type: "EuclideanLoss"
-	bottom: "steer_motor_target_data"
-	bottom: "ip2"
-	top: "euclidean"
-	loss_weight: 1
-}
-
 """
 
 
@@ -194,8 +252,12 @@ def get_model(channel=3, meta_label=6, input_width=672, input_height=376, phase=
     ZED_data = Input(shape=(channel*4,input_width, input_height), name='ZED_data')
     metadata =  Input(shape=(meta_label, input_width, input_height), name='metadata')
     steer_motor_target_data = Input(shape=(1,20), name='steer_motor_target_data')    
-   
-    conv1 = Conv2D(filters=96, kernel_size=11, strides=(3,3), padding='valid', activation='relu', data_format='channels_first', name='conv1')(ZED_data)
+    
+    ZED_data_pool1 = AveragePooling2D(pool_size=(3, 3), strides=(2,2), padding='valid', data_format='channels_first', name='ZED_data_pool1')(ZED_data)
+    ZED_data_pool2 = AveragePooling2D(pool_size=(3, 3), strides=(2,2), padding='valid', data_format='channels_first', name='ZED_data_pool1')(ZED_data_pool1)
+    ZED_data_pool2_scale = BatchNormalization(axis=1)(ZED_data_pool2)
+    
+    conv1 = Conv2D(filters=96, kernel_size=11, strides=(3,3), padding='valid', activation='relu', data_format='channels_first', name='conv1')(ZED_data_pool2_scale)
     conv1_pool = MaxPooling2D(pool_size=(3, 3), strides=(2,2), padding='valid', data_format='channels_first', name='conv1_pool')(conv1)
     
     conv1_metadata_concat = concatenate([ZED_data, metadata], axis=-3, name='conv1_metadata_concat')
