d3c4f56468a0e6d87002d4bee5e0fba724d5aa45
bdd <bdd@rfs454-122-002.path.berkeley.edu>
2017-06-22 17:33:06 -0700

    keras

--- ros/bdd_car_simple/bair_car/nodes/get_ros_images_live2_z2_color_keras_TX1.py
+++ ros/bdd_car_simple/bair_car/nodes/get_ros_images_live2_z2_color_keras_TX1.py
@@ -4,7 +4,7 @@ reed to run roslaunch first, e.g.,
 
 roslaunch bair_car bair_car.launch use_zed:=true record:=false
 """
-try:
+#try:
 
     ########################################################
     #          KARAS SETUP SECTION
@@ -274,8 +274,8 @@ try:
             unix(opjh('model_car/model_car_git_pull.sh'))
             git_pull_timer.reset()
 
-except Exception as e:
-    print("********** Exception ***********************",'red')
-    print(e.message, e.args)
-    rospy.signal_shutdown(d2s(e.message,e.args))
+#except Exception as e:
+ #   print("********** Exception ***********************",'red')
+ #   print(e.message, e.args)
+ #   rospy.signal_shutdown(d2s(e.message,e.args))
 
