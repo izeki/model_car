fec33a520ecd4c7d35c9d166f46bdeffa42db791
izeki <izeki.tw@gmail.com>
2017-08-02 13:04:29 -0700

    'keras'

--- model_car/car_run_params.py
+++ model_car/car_run_params.py
@@ -70,7 +70,8 @@ if computer_name == 'Mr_Teal':
 	#motor_gain = 1.0
 	pass
 if computer_name == 'Mr_Audi':
-	#motor_gain = 1.0
+	motor_gain = 1.0
+    motor_freeze_threshold = 70
 	pass
 if computer_name == 'Mr_Purple':
 	#motor_gain = 1.0
