51efb8afa37bab9135a176ca6ecf0311eaba4608
bdd <bdd@rfs454-122-002.path.berkeley.edu>
2017-08-22 01:45:42 -0700

    squeeznet

--- model_car/model/model.py
+++ model_car/model/model.py
@@ -207,13 +207,13 @@ def load_model_weight(model, weights_path):
     return model
     
     
-def fire(squeeze_planes, expand1x1_planes, expand3x3_planes, **kwargs):
+def fire(name, squeeze_planes, expand1x1_planes, expand3x3_planes, **kwargs):
     def f(input):
-        squeeze1x1 = Conv2D(filters=squeeze_planes, kernel_size=1, padding='valid', activation='relu', data_format='channels_first', name='squeeze1x1')(input)
-        expand1x1 = Conv2D(filters=expand1x1_planes, kernel_size=1, padding='valid', activation='relu', data_format='channels_first', name='expand1x1')(squeeze1x1)
-        expand3x3 = Conv2D(filters=expand3x3_planes, kernel_size=3, padding='valid', activation='relu', data_format='channels_first', name='expand3x3')(squeeze1x1)
+        squeeze1x1 = Conv2D(filters=squeeze_planes, kernel_size=1, padding='valid', activation='relu', data_format='channels_first', name='squeeze1x1'+name)(input)
+        expand1x1 = Conv2D(filters=expand1x1_planes, kernel_size=1, padding='valid', activation='relu', data_format='channels_first', name='expand1x1'+name)(squeeze1x1)
+        expand3x3 = Conv2D(filters=expand3x3_planes, kernel_size=3, padding='valid', activation='relu', data_format='channels_first', name='expand3x3'+name)(squeeze1x1)
         expand3x3 = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(expand3x3)
-        return concatenate([expand1x1, expand3x3], axis=1, name='concat')
+        return concatenate([expand1x1, expand3x3], axis=1, name='concat'+name)
     return f    
 
 
@@ -226,18 +226,18 @@ def get_model_squeez_net(channel=3, meta_label=6, input_width=672, input_height=
     conv1 = Conv2D(filters=64, kernel_size=2, strides=(2,2), padding='valid', activation='relu', data_format='channels_first', name='conv1')(ZED_data_pool2)
     conv1_pool = MaxPooling2D(pool_size=(2, 2), strides=(2,2), padding='valid', data_format='channels_first', name='conv1_pool')(conv1)
 
-    fire1 = fire(16, 64, 64)(conv1_pool)
-    fire2 = fire(16, 64, 64)(fire1)
+    fire1 = fire('1', 16, 64, 64)(conv1_pool)
+    fire2 = fire('2', 16, 64, 64)(fire1)
     fire_pool1 = MaxPooling2D(pool_size=(3, 3), strides=(2,2), padding='valid', data_format='channels_first', name='fire_pool1')(fire2)
     fire_pool1_metadata_concat = concatenate([fire_pool1, metadata], axis=1, name='fire_pool1_metadata_concat')    
     
-    fire3 = fire(32, 128, 128)(fire_pool1_metadata_concat)
-    fire4 = fire(32, 128, 128)(fire3)
+    fire3 = fire('3',32, 128, 128)(fire_pool1_metadata_concat)
+    fire4 = fire('4',32, 128, 128)(fire3)
     fire_pool2 = MaxPooling2D(pool_size=(3, 3), strides=(2,2), padding='valid', data_format='channels_first', name='fire_pool2')(fire4)
-    fire5 = fire(48, 192, 192)(fire_pool2)
-    fire6 = fire(48, 192, 192)(fire5)
-    fire7 = fire(64, 256, 256)(fire6)
-    fire8 = fire(64, 256, 256)(fire7)
+    fire5 = fire('5',48, 192, 192)(fire_pool2)
+    fire6 = fire('6',48, 192, 192)(fire5)
+    fire7 = fire('7',64, 256, 256)(fire6)
+    fire8 = fire('8',64, 256, 256)(fire7)
     
     drop1 = Dropout(rate=0.5, name='drop1')(fire8)
     conv2 = Conv2D(filters=2 * N_STEPS, kernel_size=1, padding='valid', data_format='channels_first', name='conv2')(drop1)
