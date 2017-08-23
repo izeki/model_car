3bd632aa23c4c83d29bd222b671e420479ef85ca
bdd <bdd@rfs454-122-002.path.berkeley.edu>
2017-08-23 15:49:22 -0700

    keras

--- model_car/model/model.py
+++ model_car/model/model.py
@@ -242,12 +242,12 @@ def get_model_squeez_net(channel=3, meta_label=6, input_width=672, input_height=
     drop1 = Dropout(rate=0.5, name='drop1')(fire8)
     conv2 = Conv2D(filters=2 * N_STEPS, kernel_size=1, padding='valid', data_format='channels_first', name='conv2')(drop1)
     avg_pool1 = AveragePooling2D(pool_size=(5, 5), strides=(6,6), padding='valid', data_format='channels_first', name='avg_pool1')(conv2)
-    reshape1 = Reshape((2*N_STEPS, -1))(avg_pool1)
+    out = Flatten()(avg_pool1)
     
     if phase == 'train':
-        model = Model(inputs=[ZED_data, metadata], outputs=reshape1) 
+        model = Model(inputs=[ZED_data, metadata], outputs=out) 
     elif phase == 'test':
-        model = Model(inputs=[ZED_data, metadata], outputs=reshape1) 
+        model = Model(inputs=[ZED_data, metadata], outputs=out) 
     else:
         model = None
     return model
